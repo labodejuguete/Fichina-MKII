48ed5fe09e46d083f88bd95c55ddc42414c764b6
labodejuguete <labodejuguete@gmail.com>
2018-07-05 19:52:34 -0300

    va

--- juegos:games/Asteroids/asteroids_font.h
+++
@@ -1,8 +0,0 @@
-#ifndef ASTEROIDS_FONT_H
-#define ASTEROIDS_FONT_H
-
-#include <avr/pgmspace.h>
-
-extern const uint8_t asteroids_font[];
-
-#endif
--- juegos:games/starfield/rocket.h
+++
@@ -1,6 +0,0 @@
-# include <avr/pgmspace.h>
-# ifndef ROCKET_H
-# define ROCKET_H
-
-extern const unsigned char rocket[];
-#endif
